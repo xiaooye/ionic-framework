af209f0952cd293d2e707db03786dc3dabe3adea
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-10 16:57:24 +0000

    chore(): add better spring explanation

--- core/src/components/modal/gestures/swipe-to-close.ts
+++ core/src/components/modal/gestures/swipe-to-close.ts
@@ -144,30 +144,74 @@ const computeDuration = (remaining: number, velocity: number) => {
 /**
  * This function lets us simulate a realistic spring-like animation
  * when swiping down on the modal.
- * We can represent the position of the spring as a function of time: x = f(t)
- * t = time, x = position
- * The derivative of the position yields the velocity.
- * The derivative of the velocity yields the acceleration.
- * We know that at t = 0, the position is 0: f(0) = 0
- * We also know that at this point, the spring does not move. Therefore,
- * we also know that the velocity is 0: f'(0) = 0.
+ * There are two forces that we need to use to compute the spring physics:
  *
- * Now, we need to figure out the acceleration which is f"(t).
- * To do this we need to define two values:
- * 1. The spring force, k: This force pulls a spring back into its equilibrium position.
- * 2. The dampening force, c: This force slows down the motion over time. Without it, a spring would oscillate forever.
+ * 1. Stiffness, k: This is a measure of resistance applied a spring.
+ * 2. Dampening, c: This value has the effect of reducing or preventing oscillation.
  *
- * We derive the following differential equation to find acceleration:
+ * Using these two values, we can calculate the Spring Force and the Dampening Force
+ * to compute the total force applied to a spring.
+ *
+ * Spring Force: This force pulls a spring back into its equilibrium position.
+ * Hooke's Law tells us that that spring force (FS) = kX.
+ * k is the stiffness of a spring, and X is the displacement of the spring from its
+ * equilibrium position. In this case, it is the amount by which the free end
+ * of a spring was displaced (stretched/pushed) from its "relaxed" position.
+ *
+ * Dampening Force: This force slows down motion. Without it, a spring would oscillate forever.
+ * The dampening force, FD, can be found via this formula: FD = -cv
+ * where c the dampening value and v is velocity.
+ *
+ * Therefore, the resulting force that is exerted on the block is:
+ * F = FS + FD = -kX - cv
+ *
+ * Newton's 2nd Law tells us that F = ma:
+ * ma = -kX - cv.
+ *
+ * For Ionic's purposes, we can assume that m = 1:
+ * a = -kX - cv
+ *
+ * Imagine a block attached to the end of a spring. At equilibrium
+ * the block is at position x = 1.
+ * Pressing on the block moves it to position x = 0;
+ * So, to calculate the displacement, we need to take the
+ * current position and subtract the previous position from it.
+ * X = x - x0 = 0 - 1 = -1.
+ *
+ * For Ionic's purposes, we are only pushing on the spring modal
+ * so we have a max position of 1.
+ * As a result, we can expand displacement to this formula:
+ * X = x - 1
+ *
+ * a = -k(x - 1) - cv
+ *
+ * We can represent the motion of something as a function of time: f(t) = x.
+ * The derivative of position gives us the velocity: f'(t)
+ * The derivative of the velocity gives us the acceleration: f''(t)
+ *
+ * We can substitute the formula above with these values:
  *
  * f"(t) = -k * (f(t) - 1) - c * f'(t)
  *
- * This value is what we plug into our `progressStep` call.
- * We cheat a bit and hardcode the formula we do not need to calculate
- * all of this on the fly:
- * f(0) = 0
- * f'(0) = 0
+ * This is called a differential equation.
+ *
+ * We know that at t = 0, we are at x = 0 because the modal does not move: f(0) = 0
+ * This means our velocity is also zero: f'(0) = 0.
+ *
+ * We can cheat a bit and plug the formula into Wolfram Alpha.
+ * However, we need to pick stiffness and dampening values:
  * k = 0.57
  * c = 15
+ *
+ * I picked these as they are fairly close to native iOS's spring effect
+ * with the modal.
+ *
+ * What we plug in is this: f(0) = 0; f'(0) = 0; f''(t) = -0.57(f(t) - 1) - 15f'(t)
+ *
+ * The result is a formula that lets us calculate the acceleration
+ * for a given time t.
+ * Note: This is the approximate form of the solution. Wolfram Alpha will
+ * give you a complex differential equation too.
  */
 const calculateSpringStep = (t: number) => {
   return 0.00255275 * 2.71828 ** (-14.9619 * t) - 1.00255 * 2.71828 ** (-0.0380968 * t) + 1
